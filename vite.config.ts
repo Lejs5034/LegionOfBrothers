ff163031f81d45348b92fdce0585df5ace91d88a
Lejs5034 <sadiclejs@gmail.com>
2025-09-21 19:58:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',   // wichtig: sagt Vercel, wo die Files landen
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  base: './', // sorgt dafür, dass Assets im richtigen Pfad geladen werden
+})
